5ada45bb4fc20c5f88b19754f6fa0e1b2ba9d293
SemihDurmus <semihdurmus@yahoo.com>
2020-10-02 12:41:07 +0200

    Edit: JS file

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const words = ["Germany", "Norway", "Spain", "England", "Greece", "Italy", "France", "Sweden", "Denmark", "Albania", "Austria", "Belgium"];
-const unwanted_chars = [8, 9, 16 ];
+const words = ["germany", "norway", "spain", "england", "greece", "italy", "france", "sweden", "denmark", "albania", "austria", "belgium"];
 
 const livesElement = document.querySelector("#remaining_lives");
 const wrongLetterElement = document.querySelector("#wrong_letters");
@@ -11,13 +10,17 @@ const randomWord = words[Math.floor(Math.random() * words.length)];
 console.log(randomWord);
 
 //Setting lives to green and display on the page
-let lives = "&#x1F7E2 &#x1F7E2 &#x1F7E2 &#x1F7E2 &#x1F7E2";
-livesElement.innerHTML = lives;
-//&#x1F534
+let lives = ["&#x1F7E2", "&#x1F7E2", "&#x1F7E2", "&#x1F7E2", "&#x1F7E2"];
+livesElement.innerHTML = lives.join("");
+
+//Variable for showing the worg letters
+wrong_array = [];
+wrongLetterElement.innerHTML = wrong_array.join(" ");
 
 //Creating an answer array of underscores and displaying on the page
 let answer_array = [];
-let wordLength = randomWord.length
+let wordLength = randomWord.length;
+
 for (i=0; i < wordLength; i++) {
     answer_array.push("_");
 };
@@ -25,24 +28,44 @@ guessAreaElement.innerHTML = answer_array.join("");
 
 
 //Getting the value of a pressed key and sending to checkLetter function for processing
+// KeyCodes a->65 z->90  
 window.addEventListener("keydown", (e) => {
-        var letter = e.key;
-        console.log(letter);
-        if (unwanted_chars.includes(e.keyCode)) {
+        const letter = e.key;
+        if ((e.keyCode < 65) || (e.keyCode > 90) ) {
             alert("Please enter a letter")
         }
-        checkLetter()
+        statusAreaElement.innerText = "";
+        checkLetter(letter)
     }
 );
 
-function checkLetter() {
+function checkLetter(x) {
     
-    if (lives.includes("&#x1F7E2") && answer_array.includes("_") ) {
-        if (randomWord.includes(letter)) {
-           // for (let i=0; i<wordLength; i++) {
-            //    if 
-            //})
+    if (lives.includes("&#x1F7E2") && answer_array.includes("_")) {
+
+        if (answer_array.includes(x) || wrong_array.includes(x)) {
+            alert("You entered '" + x + "' before.");
+        } else if (randomWord.includes(x)) {
+            for (let i=0; i<wordLength; i++) {
+                if (randomWord[i] == x) {
+                    answer_array[i] = x;
+                }
+            }
+
+            guessAreaElement.innerText = answer_array.join("");
+
+        } else {
+            lives.push("&#x1F534");
+            lives.shift();
+            livesElement.innerHTML = lives.join(" ");
+            wrong_array.push(x);
+            wrongLetterElement.innerHTML = wrong_array.join(" ");
         }
-    } 
+    } else if (! lives.includes("&#x1F7E2")) {
+        statusAreaElement.innerText = "Sorry, you could not complete the game. \nBetter luck next time"
+    }
 
 } 
+
+
+//             console.log(x.toUpperCase());
